fbd03bd5691949a0a00e5d7637652c214ce62470
debanjo31 <debanjo31@gmail.com>
2023-02-01 10:58:17 +0100

    navbar spacing done

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,19 +6,19 @@ function Navbar() {
     return (
      <div className='navbar'>
        <div className=" flex justify-around mt-4">
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col items-center	">
         <p><FaRegCompass /></p>
         <p>Explore</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
         <p><FaRegHeart /></p>
         <p>Wishlist</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
         <p><FaRegCommentDots /></p>
         <p>Messages</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
         <p><FaUserAlt /></p>
         <p>Account</p>
         </div>
