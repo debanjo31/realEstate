38cabc3c1d194685d62a7a549dfc6e7ec9ff22ca
debanjo31 <debanjo31@gmail.com>
2023-02-01 17:34:38 +0100

    color added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (
      <div className='navbar shadow-md'>
        <div className=" flex justify-around mt-4">
-        <div className="flex flex-col items-center	">
+        <div className="flex flex-col items-center	text-[#340438]">
         <p><FaRegCompass /></p>
         <p>Explore</p>
         </div>
--- src/component/TopBar.jsx
+++ src/component/TopBar.jsx
@@ -5,18 +5,18 @@ import { useRecoilState } from "recoil";
 import { darkAtom } from '../atom/darkatom';
 function TopBar() {
   const [dark, setDark] = useRecoilState(darkAtom)
-  console.log(dark)
+ 
   return (
     <div className='mt-2'>
-        <div className="flex justify-between text-lg ">
+        <div className="flex justify-between text-xl ">
             <div className="flex gap-2">
-                <FaMapMarkerAlt className='mt-1' />
+                <FaMapMarkerAlt className='mt-1 text-[#340438]' />
                 <Location />
             </div>
-            <div className="flex gap-2 mt-1">
-                { dark && <p onClick={() => setDark(!dark)}><FaMoon /></p>}
+            <div className="flex gap-6 mt-1">
+                { dark && <p onClick={() => setDark(!dark)}><FaMoon className='text-[#340438]'/></p>}
                 { !dark && <p><FaSun onClick={() => setDark(!dark)}/></p>}
-                <p><FaBell /></p>
+                <p><FaBell className='text-[#340438]'/></p>
             </div>
         </div>
     </div>
